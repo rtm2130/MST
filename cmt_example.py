23f7219c048dff0db6cfc10d201f1526c834084f
rtm2130 <rtm2130@columbia.edu>
2020-03-29 16:44:12 -0400

    CMT example now generates product features

--- cmt_example.py
+++ cmt_example.py
@@ -1,52 +1,61 @@
 '''
-This example shows how to apply the CMT tree algorithm to a simple synthetic dataset
+This example shows how to apply the MST CMT tree algorithm to a simple synthetic dataset
 The ground truth of the synethetic dataset is a CMT of depth 1
 
 We will train the CMT on this dataset and observe whether it recovers the true CMT used to generate the data
 
-X : 3 contexts:
+n = number of historical customers (i.e., training observations)
+X is a n x 3 matrix containing customers' contextual information
+3 contexts:
   X0: binary {0,1}
   X1: binary {0,1}
   X2: ordinal in {0,0.2,0.4,0.6,0.8,1}
 
-There are 5 products total. Each customer sees an assortment of 3 of these 5 products and chooses his favorite product in the assortment.
-P encodes the offered assortment: P[i] = 1 iff item i is offered to the customer
+There are 5 products total. Each customer sees a random assortment of 3 of these 5 products and chooses his favorite product in the assortment.
+P[:,:5] encodes the offered assortment: P[i,j] = 1 iff item j was offered to customer i
+There are two other product features (besides the assortment indicators) which can be interpreted as price and quality rating. 
+  Product prices are stored in P[:,5:10]. P[i,(j+5)] = price of item j offered to customer i
+  Quality ratings are stored in P[:,10:15]. P[i,(j+10)] = quality rating of item j offered to customer i
 
-Y in {0,1,2,3,4} encodes the customer's choice among the products
+Y is an n-dim vector, where Y[i] in {0,1,2,3,4} encodes customer i's choice among the products
 
 The CMT used to generate the data consists of a single split (x2 <= 0.6), with MNLs in each leaf with randomly-generated coefs
 
-NOTE: for this to run properly, include the following import statement in mtp.py: "from leaf_model_choicemod import *"
+NOTE: for this to run properly, include the following import statement in mst.py: "from leaf_model_mnl import *"
 '''
 
-from mtp import MTP
+from mst import MST
+from GenMNL import GenMNL
 import numpy as np
 import pandas as pd
 
 np.set_printoptions(suppress=True) #suppress scientific notation
+np.random.seed(0)
 
-#generates expected utilities for each product given contexts (from a CMT with split x1 <= 0.6)
-def get_expected_utilities_tree_depth_1(X, n_items):
+'''
+Generates responses Y, probability distribution of responses Y_prob given contexts X and assortments P
+
+The CMT used to generate the data consists of a single split (x2 <= 0.6), with MNLs in each leaf with randomly-generated coefs
+
+Arguments specifying MNL model type
+n_items: number of products (integer)
+num_features:  number of product features (integer), INCLUDING the binary availability feature
+model_type: whether the model has alternative varying coefficients (0) or not (1). Type integer (0/1)
+  (default is 0 meaning each alternative has a separate coeff)
+is_bias: whether the utility function has an intercept (default is True). Type boolean (True/False).
+'''
+def get_choices(X, P, n_items, num_features, model_type, is_bias):
   n = X.shape[0]
-  Beta = np.random.uniform(low=-1, high=1, size=(2,n_items))
-  U_exp = np.zeros((n,n_items))
-  for k in range(n_items):
-    U_exp[:,k] = np.where(X[:,2] <= 0.6, Beta[0,k], Beta[1,k])
-   
-  return U_exp
-
-#generates responses Y, probability distribution of responses Y_prob given contexts X and assortments P
-def get_choices(X,P):
-  scale = 10 #dictates the level of noise in the choice probabilities. epsilon~Gumbel(0,1/scale)
   
-  n = X.shape[0]
-  n_items = P.shape[1]
-  U_exp = get_expected_utilities_tree_depth_1(X, n_items)
+  left_inds = np.where(X[:,2] <= 0.6)[0]
+  right_inds = np.where(X[:,2] > 0.6)[0]
+  
+  left_mnl = GenMNL(n_items, num_features, model_type, is_bias)
+  right_mnl = GenMNL(n_items, num_features, model_type, is_bias)
   
   Y_prob = np.zeros((n,n_items))
-  denom = sum([np.exp(scale*U_exp[:,k])*P[:,k] for k in range(n_items)])
-  for k in range(n_items):
-    Y_prob[:,k] = np.where(P[:,k] == 1, np.exp(scale*U_exp[:,k])/denom, 0)
+  Y_prob[left_inds] = left_mnl.get_choice_probs(P[left_inds])
+  Y_prob[right_inds] = right_mnl.get_choice_probs(P[right_inds])
   
   Y = np.zeros(n,dtype=int)
   for i in range(n):
@@ -54,13 +63,20 @@ def get_choices(X,P):
   
   return Y, Y_prob
 
-#generates contexts X, assortments P, responses Y, response distributions Y_prob
-def generate_data(n):
+'''
+Generates contexts X, assortments P, responses Y, response probability distributions Y_prob
+
+Arguments:
+n: number of training observations (integer)
+n_items: number of products (integer)
+num_features:  number of product features (integer), INCLUDING the binary availability feature
+model_type: whether the model has alternative varying coefficients (0) or not (1). Type integer (0/1)
+  (default is 0 meaning each alternative has a separate coeff)
+is_bias: whether the utility function has an intercept (default is True). Type boolean (True/False).
+'''
+def generate_data(n, n_items, assortment_size, num_features, model_type, is_bias):
   
-  n_items = 5
-  assortment_size = 3
-  
-  #CUSTOMER FEATURES
+  #GENERATE CUSTOMER FEATURES
   #X1 is in {0,1} (binary)
   X1 = np.random.choice([0,1], size=n, replace=True).reshape((n,1))
   #X2 is in {0,1} (binary)
@@ -69,16 +85,17 @@ def generate_data(n):
   X3 = np.random.choice([0,0.2, 0.4, 0.6, 0.8, 1], size=n, replace=True).reshape((n,1))
   X = np.concatenate((X1,X2,X3),axis = 1)
   
-  #ASSORTMENT (5 products, choose 3 to offer to each customer)
-  P = np.zeros((n,n_items))
+  #GENERATE ASSORTMENT AND PRODUCT FEATURES 
+  P = np.zeros((n,num_features*n_items))
   for i in range(n):
+    #generate assortment features (5 products, choose 3 to offer to each customer)
     assortment_items = np.random.choice(range(n_items), size=assortment_size, replace=False)
     P[i,assortment_items] = 1
+    #generate price and quality rating features
+    P[i,n_items:] = np.random.uniform(size=(num_features-1)*n_items)
   
-  #P = np.ones((n,n_items))
-  
-  #OUTCOME
-  Y, Y_prob = get_choices(X,P)
+  #GENERATE OUTCOMES (response probability distributions Y_prob, observed responses Y)
+  Y, Y_prob = get_choices(X, P, n_items, num_features, model_type, is_bias)
   
   return X,P,Y,Y_prob
   
@@ -88,21 +105,30 @@ n_train = 5000;
 n_valid = 2500;
 n_test = 2000;
 
-#GENERATE DATA
+n_items = 5
+assortment_size = 3
+num_features = 3
+model_type = 0
+is_bias = True
 
-X,P,Y,Y_prob = generate_data(n_train+n_valid+n_test)
+#GENERATE DATA
+X,P,Y,Y_prob = generate_data(n_train+n_valid+n_test, n_items, assortment_size, num_features, model_type, is_bias)
 XV,PV,YV,Y_probV = X[n_train:(n_train+n_valid)],P[n_train:(n_train+n_valid)],Y[n_train:(n_train+n_valid)],Y_prob[n_train:(n_train+n_valid)] #valid set
 XT,PT,YT,Y_probT = X[(n_train+n_valid):],P[(n_train+n_valid):],Y[(n_train+n_valid):],Y_prob[(n_train+n_valid):]  #test set
 X,P,Y,Y_prob = X[:n_train],P[:n_train],Y[:n_train],Y_prob[:n_train] #training set
 
 #APPLY TREE ALGORITHM. TRAIN TO DEPTH 1
-my_tree = MTP(max_depth = 2, min_weights_per_node = 100, quant_discret = 0.05)
-my_tree.fit(X,P,Y,verbose=True,
+my_tree = MST(max_depth = 2, min_weights_per_node = 100, quant_discret = 0.05)
+my_tree.fit(X,P,Y,verbose=False,
                 feats_continuous=[False, False, True],
                 refit_leaves=True,
-                num_features = 1, mode = "mnl", batch_size = 100, epochs = 100, 
-                steps = 5000, is_bias = True, model_type = 0);
-
+                num_features = num_features, is_bias = is_bias, model_type = model_type,
+                mode = "mnl", batch_size = 100, epochs = 100, steps = 5000,
+                leaf_mod_thresh=10000000);
+#ABOVE: leaf_mod_thresh controls whether when fitting a leaf node we apply Newton's method or stochastic gradient descent.
+# If the number of training observations in a leaf node <= leaf_mod_thresh, then newton's method
+# is applied; otherwise, stochastic gradient descent is applied.
+            
 #PRINT OUT THE UNPRUNED TREE. OBSERVE THAT THE FIRST SPLIT IS CORRECT, BUT THERE ARE UNNECESSARY SPLITS AFTER THAT
 my_tree.traverse(verbose=True)   
 #PRUNE THE TREE    
